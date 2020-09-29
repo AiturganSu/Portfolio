26129e7ec5f8a6338cf379da5a9fde32534311c6
Aiturgan Suiunbai kyzy <aiturgansuiunbaikyzy@Aiturgans-MacBook-Pro.local>
2020-09-28 22:16:45 -0400

    added express js support

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//About Page
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+// Products Page
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+
+//Services Page
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+//Contact page
+router.get('/contacts', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
 module.exports = router;
