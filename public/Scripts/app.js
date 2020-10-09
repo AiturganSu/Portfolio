54530f3788217095887ab9b29dd093a4843958a3
Aiturgan Suiunbai kyzy <aiturgansuiunbaikyzy@Aiturgans-MacBook-Pro.local>
2020-10-10 00:01:24 +0600

    Addded services ejs, about me ppage is almost done except buttons

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -8,9 +8,23 @@
         if (document.title == "Contact") {
             let sendButton = document.getElementById("sendButton");
             let cancelButton = document.getElementById("cancelButton");
+            let form = document.forms[0];
 
             sendButton.addEventListener("click", (event) => {
                 event.preventDefault();
+
+                let firstName = document.getElementById("fullName").value;
+                let contactNumer = document.getElementById("contactNumber").value;
+                let emailAddress = document.getElementById("email")
+                let message = document.getElementById("message").value;
+
+                console.info(`Full Name: ${fullName}
+                Contact Number: ${contactNumer}
+                Email Address:  ${emailAddress}
+                Message:        ${message}`);
+
+                form.reset();
+
             })
 
             cancelButton.addEventListener("click", (event) => {
