56bcf6815e289a0147aee5836ddd312ed490d8a5
Aiturgan Suiunbai kyzy <aiturgansuiunbaikyzy@Aiturgans-MacBook-Pro.local>
2020-10-10 05:20:38 +0600

    one cv button is working, the one on about me page

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,32 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 //About Page
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+    res.render('about', { title: 'About' });
 });
 
 // Projects Page
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Products' });
+    res.render('projects', { title: 'Products' });
 });
 
 //Services Page
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services' });
+    res.render('services', { title: 'Services' });
 });
 
 //Contact page
 router.get('/contacts', function(req, res, next) {
-  res.render('contact', { title: 'Contact' });
+    res.render('contact', { title: 'Contact' });
+});
+
+//Resume page
+router.get('/resume', function(req, res, next) {
+    res.render('resume', { title: 'resume' });
 });
 
 module.exports = router;
