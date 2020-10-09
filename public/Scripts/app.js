95fce620d725e7f6d3dc3101ebf38bc9e2c095a8
Aiturgan Suiunbai kyzy <aiturgansuiunbaikyzy@Aiturgans-MacBook-Pro.local>
2020-10-09 14:20:47 +0600

    added contact form and buttons like send and cancel

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,12 +1,27 @@
-
 //IIFE - Immadietly invoked Function Expression
-(function(){
+(function() {
 
-    function Start(){
-       console.log(`%c App Started...`, 
-       "fonr-size: 20px; color: blue; font-weight: bold");
-     }
+    function Start() {
+        console.log(`%c App Started...`,
+            "fonr-size: 20px; color: blue; font-weight: bold");
+
+        if (document.title == "Contact") {
+            let sendButton = document.getElementById("sendButton");
+            let cancelButton = document.getElementById("cancelButton");
+
+            sendButton.addEventListener("click", (event) => {
+                event.preventDefault();
+            })
+
+            cancelButton.addEventListener("click", (event) => {
+                event.preventDefault();
+                if (confirm("Are you sure?")) {
+                    location.href = "/";
+                }
+            })
+
+        }
+    }
 
     window.addEventListener("load", Start);
-}
-)();
+})();
